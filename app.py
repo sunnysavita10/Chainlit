c656062de7eee1f445699ee02297f46d4623edf9
sunnysavita10 <engsunny3365@gmail.com>
2024-05-05 11:11:28 +0530

    text spillter updated

--- app.py
+++ app.py
@@ -24,3 +24,32 @@ OPENAI_API_KEY=os.getenv("OPENAI_API_KEY")
 
 os.environ["OPENAI_API_KEY"] = OPENAI_API_KEY
 
+
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=100)
+
+
+@cl.on_chat_start
+async def on_chat_start():
+    files = None
+
+    # Wait for the user to upload a file
+    while files == None:
+        files = await cl.AskFileMessage(
+            content="Please upload a text file to begin!",
+            accept=["text/plain"],
+            max_size_mb=20,
+            timeout=180,
+        ).send()
+
+    file = files[0]
+    
+    msg = cl.Message(content=f"Processing `{file.name}`...", disable_feedback=True)
+    await msg.send()
+    
+    with open(file.path, "r", encoding="utf-8") as f:
+        text = f.read()
+
+    # Split the text into chunks
+    texts = text_splitter.split_text(text)
+
+    
