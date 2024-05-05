fbf79b585062363b1165f5fa5ffc80913bcf035a
sunnysavita10 <engsunny3365@gmail.com>
2024-05-05 11:02:40 +0530

    code upadted

--- app.py
+++ app.py
@@ -15,3 +15,12 @@ from langchain.memory import ChatMessageHistory, ConversationBufferMemory
 import chainlit as cl
 
 print("all_ok")
+
+from dotenv import load_dotenv
+
+load_dotenv() 
+
+OPENAI_API_KEY=os.getenv("OPENAI_API_KEY")
+
+os.environ["OPENAI_API_KEY"] = OPENAI_API_KEY
+
