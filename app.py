9d6ddee546d8e25c13c29eeb022e9d3577b4ad08
sunnysavita10 <engsunny3365@gmail.com>
2024-05-05 10:57:40 +0530

    code updated

---
+++ app.py
@@ -0,0 +1,17 @@
+import os
+from typing import List
+
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.vectorstores import Chroma
+from langchain.chains import (
+    ConversationalRetrievalChain,
+)
+from langchain.chat_models import ChatOpenAI
+
+from langchain.docstore.document import Document
+from langchain.memory import ChatMessageHistory, ConversationBufferMemory
+
+import chainlit as cl
+
+print("all_ok")
